5b90394dce628d065b608931bd23ec953b05c9dd
John Haroian <john@haroian.net>
2024-12-21 22:53:07 -0800

    Update app-gum to handle either x86/x64 or arm64

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,7 +1,7 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
-cd /tmp
-GUM_VERSION="0.14.3" # Use known good version
-wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_amd64.deb"
-sudo apt-get install -y ./gum.deb
-rm gum.deb
-cd -
+
+# From Gum github site for installing on Ubuntu.  Respects architecture
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install gum
